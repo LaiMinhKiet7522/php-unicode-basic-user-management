f0ee8ff6b1fcffe271763b9f18883038905e1cee
Minh Kiet <kietminh070502@gmail.com>
2023-10-23 20:28:14 +0700

    Forgot password function

--- modules/auth/forgot.php
+++ modules/auth/forgot.php
@@ -1,5 +1,84 @@
 <?php
-if(!defined('_INCODE')){
+if (!defined('_INCODE')) {
     die('Access denied');
-} 
+}
+$data = [
+    'pageTitle' => 'Đặt lại mật khẩu'
+];
+layout('header-login', $data);
+//Kiểm tra trạng thái đăng nhập
+if (isLogin()) {
+    redirect('?module=users');
+}
+//Xử lý đăng nhập
+if (isPost()) {
+    $body = getBody();
+    if (!empty($body['email'])) {
+        $email = $body['email'];
+        $queryUser = firstRaw("SELECT id FROM users WHERE email='$email'");
+        if (!empty($queryUser)) {
+            $userId = $queryUser['id'];
+
+            //Tạo forgotToken 
+            $forgotToken = sha1(uniqid() . time());
+            $dataUpdate = [
+                'forgotToken' => $forgotToken
+            ];
+            $updateStatus = update('users', $dataUpdate, "id=$userId");
+            if ($updateStatus) {
+                //Tạo link khôi phục
+                $linkReset = _WEB_HOST_ROOT . '?module=auth&action=reset&token=' . $forgotToken;
+                //Thiết lập gửi mail
+                $subject = 'Yêu cầu khôi phục mật khẩu';
+                $content = 'Chào bạn: ' . $email.'<br />';
+                $content .= 'Chúng tôi nhận được yêu cầu khôi phục mật khẩu từ bạn, vui lòng click vào link sau để khôi phục <br />';
+                $content .= $linkReset . '<br />';
+                $content .= 'Trân trọng!';
+                //Tiến hành gửi mail
+                $sendStatus = sendMail($email, $subject, $content);
+                if ($sendStatus) {
+                    setFlashData('msg', 'Vui lòng kiểm tra email để xem hướng dẫn đặt lại mật khẩu.');
+                    setFlashData('msg_type', 'success');
+                } else {
+                    setFlashData('msg', 'Có lỗi xảy ra trong quá trình gửi Mail! Vui lòng thử lại sau.');
+                    setFlashData('msg_type', 'danger');
+                }
+            } else {
+                setFlashData('msg', 'Lỗi hệ thống! Vui lòng thử lại sau.');
+                setFlashData('msg_type', 'danger');
+            }
+        } else {
+            setFlashData('msg', 'Địa chỉ email không tồn tại trong hệ thống! Vui lòng kiểm tra lại.');
+            setFlashData('msg_type', 'danger');
+        }
+    } else {
+        setFlashData('msg', 'Vui lòng nhập địa chỉ email');
+        setFlashData('msg_type', 'danger');
+    }
+    redirect('?module=auth&action=forgot');
+}
+$msg = getFlashData('msg');
+$msg_type = getFlashData('msg_type');
+$old = getFlashData('old');
+?>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+<div class="row">
+    <div class="col-6" style="margin: 20px auto;">
+        <h3 class="text-center text-uppercase">Đặt lại mật khẩu</h3>
+        <?php getMessage($msg, $msg_type); ?>
+        <form action="" method="post">
+            <div class="form-group">
+                <label for="">Email</label>
+                <input type="text" class="form-control" name="email" placeholder="Địa chỉ email..." value="<?php echo old('email', $old); ?>">
+            </div>
+            <button type="submit" class="btn btn-primary btn-block">Xác nhận</button>
+            <hr>
+            <p class="text-center"><a href="?module=auth&action=login">Đăng nhập</a></p>
+            <p class="text-center"><a href="?module=auth&action=register">Đăng ký tài khoản</a></p>
+        </form>
+    </div>
+</div>
+<?php
+layout('footer-login');
 ?>
--- modules/auth/reset.php
+++ modules/auth/reset.php
@@ -1,5 +1,146 @@
 <?php
-if(!defined('_INCODE')){
+if (!defined('_INCODE')) {
     die('Access denied');
-} 
+}
+layout('header-login');
+echo '<div class="container text-center"><br />';
+$token = getBody()['token'];
+if (!empty($token)) {
+    //Truy vấn kiểm tra token với database
+    $tokenQuery = firstRaw("SELECT id, email FROM users WHERE forgotToken = '$token'");
+    if (!empty($tokenQuery)) {
+        $userId = $tokenQuery['id'];
+        $userEmail = $tokenQuery['email'];
+        if (isPost()) {
+            $body = getBody();
+
+            $errors = []; //Mảng chứa các lỗi
+
+            //Validate Mật khẩu: Bắt buộc nhập, Độ dài lớn hơn hoặc bằng 8 ký tự
+            if (empty(trim($body['password']))) {
+                $errors['password']['required'] = 'Vui lòng nhập mật khẩu mới';
+            } else {
+                if (strlen(trim($body['password'])) < 8) {
+                    $errors['password']['min'] = 'Mật khẩu không được nhỏ hơn 8 ký tự';
+                }
+            }
+
+            //Validate Nhập lại Mật khẩu: Bắt buộc nhập, phải giống trường mật khẩu đã nhập trước đó
+            if (empty(trim($body['confirm_password']))) {
+                $errors['confirm_password']['required'] = 'Vui lòng nhập lại xác nhận mật khẩu mới';
+            } else {
+                if (trim($body['password']) != trim($body['confirm_password'])) {
+                    $errors['confirm_password']['match'] = 'Hai mật khẩu không khớp nhau';
+                }
+            }
+
+            if (empty($errors)) {
+                //Xử lý update mật khẩu
+                $passwordHash = password_hash($body['password'], PASSWORD_DEFAULT);
+                $dataUpdate = [
+                    'password' => $passwordHash,
+                    'forgotToken' => NULL,
+                    'updateAt' => date('Y-m-d H:i:s')
+                ];
+                $updateStatus = update('users', $dataUpdate, "id=$userId");
+                if ($updateStatus) {
+                    setFlashData('msg', 'Thay đổi mật khẩu thành công!');
+                    setFlashData('msg_type', 'success');
+
+                    //Gửi mail thông báo đổi mật khẩu thành công
+                    $subject = 'Bạn vừa đổi mật khẩu';
+                    $content = 'Chúc mừng bạn đã đổi mật khẩu thành công! <br/>';
+                    sendMail($userEmail, $subject, $content);
+                    redirect('?module=auth&action=login');
+                } else {
+                    setFlashData('msg', 'Có lỗi xảy ra! Vui lòng thử lại sau.');
+                    setFlashData('msg_type', 'danger');
+                    redirect('?module=auth&action=reset&token=' . $token);
+                }
+            } else {
+                //Có lỗi xảy ra
+                setFlashData('msg', 'Vui lòng kiểm tra dữ liệu nhập vào');
+                setFlashData('msg_type', 'danger');
+                setFlashData('errors', $errors);
+                redirect('?module=auth&action=reset&token=' . $token); //Load lại trang reset password   
+            }
+        }
+        $msg = getFlashData('msg');
+        $msg_type = getFlashData('msg_type');
+        $errors = getFlashData('errors');
+?>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+        <div class="row text-left">
+            <div class="col-6" style="margin: 20px auto;">
+                <h3 class="text-center text-uppercase">Đặt lại mật khẩu</h3>
+                <?php
+                getMessage($msg, $msg_type);
+                ?>
+                <form action="" method="post">
+                    <div class="form-group">
+                        <label>Mật khẩu mới</label>
+                        <div class="input-group" id="show_hide_password">
+                            <input type="password" name="password" class="form-control" id="password" placeholder="Mật khẩu mới...">
+                            <a href="javascript:;" class="input-group-text bg-transparent"><i style="width: 25px;" class='fa-solid fa-eye-slash'></i></a>
+                        </div>
+                        <?php echo form_error('password', $errors, '<span class="error">', '</span>'); ?>
+                    </div>
+                    <div class="form-group">
+                        <label>Nhập lại mật khẩu mới</label>
+                        <div class="input-group" id="show_hide_confirm_password">
+                            <input type="password" name="confirm_password" class="form-control" id="confirm_password" placeholder="Nhập lại mật khẩu mới...">
+                            <a href="javascript:;" class="input-group-text bg-transparent"><i style="width: 25px;" class='fa-solid fa-eye-slash'></i></a>
+                        </div>
+                        <?php echo form_error('confirm_password', $errors, '<span class="error">', '</span>'); ?>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-block">Xác nhận</button>
+                    <hr>
+                    <p class="text-center"><a href="?module=auth&action=login">Đăng nhập</a></p>
+                    <p class="text-center"><a href="?module=auth&action=register">Đăng ký tài khoản</a></p>
+                    <input type="hidden" name="token" value="<?php echo $token; ?>">
+                </form>
+            </div>
+        </div>
+        <script>
+            $(document).ready(function() {
+                $("#show_hide_password a").on('click', function(event) {
+                    event.preventDefault();
+                    if ($('#show_hide_password input').attr("type") == "text") {
+                        $('#show_hide_password input').attr('type', 'password');
+                        $('#show_hide_password i').addClass("fa-eye-slash");
+                        $('#show_hide_password i').removeClass("fa-eye");
+                    } else if ($('#show_hide_password input').attr("type") == "password") {
+                        $('#show_hide_password input').attr('type', 'text');
+                        $('#show_hide_password i').removeClass("fa-eye-slash");
+                        $('#show_hide_password i').addClass("fa-eye");
+                    }
+                });
+            });
+        </script>
+        <script>
+            $(document).ready(function() {
+                $("#show_hide_confirm_password a").on('click', function(event) {
+                    event.preventDefault();
+                    if ($('#show_hide_confirm_password input').attr("type") == "text") {
+                        $('#show_hide_confirm_password input').attr('type', 'password');
+                        $('#show_hide_confirm_password i').addClass("fa-eye-slash");
+                        $('#show_hide_confirm_password i').removeClass("fa-eye");
+                    } else if ($('#show_hide_confirm_password input').attr("type") == "password") {
+                        $('#show_hide_confirm_password input').attr('type', 'text');
+                        $('#show_hide_confirm_password i').removeClass("fa-eye-slash");
+                        $('#show_hide_confirm_password i').addClass("fa-eye");
+                    }
+                });
+            });
+        </script>
+<?php
+    } else {
+        getMessage('Liên kết không tồn tại hoặc đã hết hạn', 'danger');
+    }
+} else {
+    getMessage('Liên kết không tồn tại hoặc đã hết hạn', 'danger');
+}
+echo '</div>';
+layout('footer-login');
 ?>
